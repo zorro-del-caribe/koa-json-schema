c9f2388743e3ba62acac2738fa194e22900b44a0
lorenzofox3 <laurent34azerty@gmail.com>
2018-02-26 11:16:48 -0500

    update dep

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 const assert = require('assert');
 const Ajv = require('ajv');
 
-module.exports = function (schema, options = {}) {
-  assert(schema, 'schema must be provided');
-  const ajv = new Ajv(options);
-  const validate = ajv.compile(schema);
+module.exports = (schema, options = {}) => {
+	assert(schema, 'schema must be provided');
+	const ajv = new Ajv(options);
+	const validate = ajv.compile(schema);
 
-  return async function (ctx, next) {
-    const data = Object.assign({}, ctx.request.query, ctx.request.body, ctx.params);
-    const isValid = validate(data);
-    if (!isValid) {
-      ctx.throw(422, 'invalid inputs', {error_description: validate.errors});
-    }
-    await next();
-  }
+	return async (ctx, next) => {
+		const data = Object.assign({}, ctx.request.query || {}, ctx.request.body || {}, ctx.params || {});
+		const isValid = validate(data);
+		if (!isValid) {
+			ctx.throw(422, 'invalid inputs', {error_description: validate.errors});
+		}
+		await next();
+	};
 };
